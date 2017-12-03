1943054a503e0e82f456c69410c5f771f0cff54d
Julian Doherty (madlep) <madlep@madlep.com>
2017-12-04 00:27:47 +1100

    day 3 part 2

--- lib/aoc_17/day_3.ex
+++ lib/aoc_17/day_3.ex
@@ -6,7 +6,8 @@ defmodule Aoc17.Day3 do
       heading: :right,
       width: 1,
       progress: 1,
-      side: 1
+      side: 1,
+      data: %{ {0,0} => 1}
     ]
 
     def step(%State{
@@ -14,13 +15,16 @@ defmodule Aoc17.Day3 do
       pos: pos,
       width: width,
       progress: progress,
-      heading: heading
+      heading: heading,
+      data: data
     } = state)
     when progress < width do
+      new_pos = move(pos, heading)
       %State{state|
         n: n+1,
-        pos: move(pos, heading),
-        progress: progress + 1
+        pos: new_pos,
+        progress: progress + 1,
+        data: Map.put(data, new_pos, adjacent_sum(new_pos, data))
       }
     end
 
@@ -30,19 +34,26 @@ defmodule Aoc17.Day3 do
       width: width,
       progress: progress,
       heading: heading,
-      side: side
+      side: side,
+      data: data
     } = state)
     when progress == width do
       new_heading = rotate(heading)
+      new_pos = move(pos, heading)
       %State{state|
         n: n + 1,
-        pos: move(pos, heading),
+        pos: new_pos,
         progress: 1,
         width: grow_width(width, side),
         side: side + 1,
-        heading: new_heading
+        heading: new_heading,
+        data: Map.put(data, new_pos, adjacent_sum(new_pos, data))
       }
     end
+
+    def distance(%State{pos: {x, y}}), do: abs(x) + abs(y)
+
+    def data(%State{pos: pos, data: data}), do: data[pos]
 
     defp move({x,y}, :right), do: {x+1,  y}
     defp move({x,y}, :up)   , do: {x,    y-1}
@@ -57,7 +68,20 @@ defmodule Aoc17.Day3 do
     defp grow_width(width, side) when rem(side, 2) == 0, do: width + 1
     defp grow_width(width, side) when rem(side, 2) != 0, do: width
 
-    def distance(%State{pos: {x, y}}), do: abs(x) + abs(y)
+    defp adjacent_sum({x,y}, data) do
+      [
+        {x-1, y-1},
+        {x,   y-1},
+        {x+1, y-1},
+        {x-1, y},
+        {x, y},
+        {x+1, y},
+        {x-1, y+1},
+        {x,   y+1},
+        {x+1, y+1}
+      ]
+      |> Enum.reduce(0, fn(pos, sum) -> (data[pos] || 0) + sum end)
+    end
   end
 
   @doc """
@@ -74,11 +98,41 @@ defmodule Aoc17.Day3 do
     31
   """
   def part1(input) do
+    at(input)
+    |> State.distance
+  end
+
+  @doc """
+    iex> Aoc17.Day3.part2(1)
+    2
+
+    iex> Aoc17.Day3.part2(2)
+    4
+
+    iex> Aoc17.Day3.part2(3)
+    4
+
+    iex> Aoc17.Day3.part2(4)
+    5
+
+    iex> Aoc17.Day3.part2(5)
+    10
+  """
+  def part2(input) do
+    stream()
+    |> Enum.find(fn(state) -> State.data(state) > input end)
+    |> State.data
+  end
+
+  defp stream do
     Stream.unfold(%State{}, fn(state) ->
       {state, State.step(state)}
     end)
+  end
+
+  def at(input) do
+    stream()
     |> Enum.at(input - 1)
-    |> State.distance
   end
 
 end
