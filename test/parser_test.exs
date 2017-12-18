bf41ee84bf40da49d77fc3b90bb8da5863607352
Julian Doherty (madlep) <madlep@madlep.com>
2017-12-18 21:06:18 +1100

    test for multiple lines

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -17,4 +17,28 @@ defmodule ParserTest do
     assert parse(parser, "fwft (72) -> ktlj, cntj, xhth") ==
       {["fwft", 72, ["ktlj", "cntj", "xhth"]], ""}
   end
+
+  test "can parse AOC day 7 multiple lines" do
+    import Parser
+
+    parser = sep_by(
+      word()
+      |> skip(space())
+      |> between(char("("), integer(), char(")"))
+      |> option(
+        skip(trim(string("->")))
+        |> sep_by(word(), trim(char(",")), at_least_one: true)
+      ),
+      newline()
+    )
+
+    assert parse(parser, "pbga (66)\nfwft (72) -> ktlj, cntj, xhth") ==
+      {
+        [
+          ["pbga", 66, [] ],
+          ["fwft", 72, ["ktlj", "cntj", "xhth"]]
+        ],
+        ""
+      }
+  end
 end
